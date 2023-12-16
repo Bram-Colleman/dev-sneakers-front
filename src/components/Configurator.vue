7ef0b45d952c945401a3cf6ac7550341dba17ee0
Bram-Colleman <r0820751@student.thomasmore.be>
2023-12-16 16:51:53 +0100

    feat: add color lists and cycle through categories method

--- src/components/Configurator.vue
+++ src/components/Configurator.vue
@@ -2,17 +2,57 @@
 import { ref, onMounted } from 'vue';
 
 const outside1 = ref(["red", "green", "blue", "purple", "yellow"]);
+const outside2 = ref(["red", "green", "blue", "purple", "yellow"]);
+const laces = ref(["red", "green", "blue", "purple", "yellow"]);
+const sole1 = ref(["red", "green", "blue", "purple", "yellow"]);
+const sole2 = ref(["red", "green", "blue", "purple", "yellow"]);
+const inside = ref(["red", "green", "blue", "purple", "yellow"]);
+const categories = ref(["outside1", "outside2", "laces", "sole1", "sole2", "inside"]);
+const current = ref(0);
+
+const cycle = (factor) => {
+    if (factor == 1) {
+        if (current.value == categories.value.length - 1) {
+            current.value = 0;
+        } else {
+            current.value++;
+        }
+    } else {
+        if (current.value == 0) {
+            current.value = categories.value.length - 1;
+        } else {
+            current.value--;
+        }
+    }
+};
+
+
+const c_outside1 = ref("");
+
+// {
+//     "brand": "nike",
+//     "outside_1Color": "red",
+//     "outside_2Color": "green",
+//     "lacesColor": "blue",
+//     "sole_1Color": "black",
+//     "sole_2Color": "white",
+//     "insideColor": "yellow",
+//     "size": 8,
+//     "price": 8,
+//     "status": "delivery"
+
+// }
 
 </script>
 <template>
     <div>
-        <button>test</button>
-        <span>test</span>
-        <button>test</button>
+        <button @click="cycle(-1)">prev</button>
+        <span>{{ categories[current] }}</span>
+        <button @click="cycle(1)">next</button>
     </div>
     <div class="colors">
         <div v-for="c in outside1">
-            <input type="radio" :name=c :id=c >
+            <input type="radio" name="c_outside1" :id=c v-model="c_outside1">
             <label :for=c >{{ c }}</label>
         </div>
     </div>
