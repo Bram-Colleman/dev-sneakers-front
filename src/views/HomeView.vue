72e0f24ccfa83b02a0d72ccd85eb7588cf3d461c
Bram Colleman <bram.colleman@telenet.be>
2023-12-18 13:53:01 +0100

    faet: add logo to homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,17 @@ import Configurator from '@/components/Config.vue';
     <div class="nav">
       <router-link to="/login">Login</router-link>
     </div>
+    <img src="/media/logo.webp" alt="SWEAR" id="logo">
     <Configurator></Configurator>
   </main>
 </template>
 
 <style scoped>
+#logo {
+  position: absolute;
+  margin: 1rem 5rem;
+  filter: invert(1);
+}
 canvas {
   outline: 1px solid black;
 }
@@ -19,6 +25,7 @@ canvas {
   display: flex;
   justify-content: flex-end;
   padding: 0 2rem;
+  background-color: #69FF47;
 }
 a {
   color: black;
