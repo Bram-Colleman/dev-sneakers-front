be80e3bbe0f181a4b46ebc7be51b97879b59512a
Bram Colleman <bram.colleman@telenet.be>
2023-12-19 18:07:16 +0100

    fix: add return to sockets.on

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -15,7 +15,7 @@ let socket = Primus.connect("https://dev5-sneaker-api.onrender.com/", {
   reconnect: {
     max: Infinity, // Number: The max delay before we try to reconnect.
     min: 500, // Number: The minimum delay before we try reconnect.
-    retries: 100, // Number: How many times we should try to reconnect.
+    retries: 10, // Number: How many times we should try to reconnect.
   },
 });
 
